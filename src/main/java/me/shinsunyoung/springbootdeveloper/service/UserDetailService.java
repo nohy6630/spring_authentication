ddc90b344c5f5055c86795d34553dca061a9850c
nohy6630 <nohy6630@gmail.com>
2023-07-26 15:30:25 +0900

    feat: 시큐리티 설정

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package me.shinsunyoung.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
