9898241ab7fb2a9bfff0bd5f37d313997f1a9687
nohy6630 <nohy6630@gmail.com>
2023-07-27 22:03:12 +0900

    feat: 로그인 로그아웃 기능 구현

--- src/main/java/me/shinsunyoung/springbootdeveloper/service/UserDetailService.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/service/UserDetailService.java
@@ -1,8 +1,8 @@
 package me.shinsunyoung.springbootdeveloper.service;
 
 import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.User;
 import me.shinsunyoung.springbootdeveloper.repository.UserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -13,7 +13,7 @@ public class UserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
     }
